13ccec9f5a301bd27af2b37e3dc0518df5356579
Pranavkrishnan007 <pranavk0217@gmail.com>
2022-07-04 15:09:46 +0530

    2nd iteration

--- S2_ESL_project/S2_ESL_project.ino
+++ S2_ESL_project/S2_ESL_project.ino
@@ -17,51 +17,64 @@ void setup()
   pinMode(11,OUTPUT);
 }
 
+int door = 0, light = 0;
+
 void loop()
 {
   LDR=map(analogRead(A1),0,1023,0,255);
-  if (BTSerial.listen()>0 && LDR <=50)//where value == the value we get in adjusting LDR
+  if (BTSerial.listen()>0)//where value == the value we get in adjusting LDR
   {
-    int count=0;
-    if (count==0){
-    Serial.println("WELCOME.....THE LIGHTS ARE TURNED ON");
-    BTSerial.write("WELCOME.....THE LIGHTS ARE TURNED ON");
-    count+=1;}
-    digitalWrite(13,HIGH);//Yellow light
-    digitalWrite(12,HIGH);//Red light
-    //digitalWrite(11,HIGH); 
-    if (BTSerial.available()>0){
-      bluetooth=BTSerial.read();
-      if (bluetooth=='o')
-      {
-        for(int rotate=0;rotate<=180;rotate++)
-        {
-          myservo.write(rotate);
-        }
-        Serial.println("The Door is Opened");
-        BTSerial.println("The Door is Opened");
+    if(LDR >= 100){
+      if(light == 0){
+        Serial.println("WELCOME.....THE LIGHTS ARE TURNED ON");
+        BTSerial.write("WELCOME.....THE LIGHTS ARE TURNED ON");
+        digitalWrite(13,HIGH);//Yellow light
+        digitalWrite(12,HIGH);//Red light
+        light = 1;
       }
-      else if(bluetooth=='f')
-      {
-        for (int rotate=180;rotate>=0;rotate--)
-        {
-          myservo.write(rotate);
-        }
-        Serial.println("The Door is closed");
-        BTSerial.println("The Door is closed");
-      }
-      else if (bluetooth=='O')
-      {
-        digitalWrite(12,HIGH);
-        digitalWrite(13,LOW);
-        printf("The Red Light is on");
-      }
-      else if(bluetooth=='F')
-      {
-        digitalWrite(13,HIGH);
-        digitalWrite(12,LOW);
-        BTSerial.println("The Yellow Light is on");
-        Serial.println("The Yellow light is on:");
-      }
+    }
+    else
+    // if(light == 0){
+    //   Serial.println("WELCOME.....THE LIGHTS ARE TURNED ON");
+    //   BTSerial.write("WELCOME.....THE LIGHTS ARE TURNED ON");
+    //   digitalWrite(13,HIGH);//Yellow light
+    //   digitalWrite(12,HIGH);//Red light
+    //   light = 1;
+    // }
+    // //digitalWrite(11,HIGH); 
+    // if (BTSerial.available()>0){
+    //   bluetooth=BTSerial.read();
+    //   if (bluetooth=='o')
+    //   {
+    //     for(int rotate=0;rotate<=180;rotate++)
+    //     {
+    //       myservo.write(rotate);
+    //     }
+    //     Serial.println("The Door is Opened");
+    //     BTSerial.println("The Door is Opened");
+    //   }
+    //   else if(bluetooth=='f')
+    //   {
+    //     for(int rotate=180;rotate>=0;rotate--)
+    //     {
+    //       myservo.write(rotate);
+    //     }
+    //     Serial.println("The Door is closed");
+    //     BTSerial.println("The Door is closed");
+    //   }
+    //   else if (bluetooth=='O')
+    //   {
+    //     digitalWrite(12,HIGH);
+    //     digitalWrite(13,LOW);
+    //     printf("The Red Light is on");
+    //   }
+    //   else if(bluetooth=='F')
+    //   {
+    //     digitalWrite(13,HIGH);
+    //     digitalWrite(12,LOW);
+    //     BTSerial.println("The Yellow Light is on");
+    //     Serial.println("The Yellow light is on:");
+    //   }
+    // }
   }
 }
